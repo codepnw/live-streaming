1548674156fb7c32ccc203d773c97e84450e97ed
codepnw <idchokms01@outlook.com>
2023-11-09 18:55:13 +0700

    pkg: setup websocket

---
+++ pkg/ws/hub.go
@@ -0,0 +1,40 @@
+package ws
+
+type Hub struct {
+	clients    map[*Client]bool
+	register   chan *Client
+	unRegister chan *Client
+	broadcast  chan []byte
+}
+
+func NewHub() *Hub {
+	return &Hub{
+		clients:    make(map[*Client]bool),
+		register:   make(chan *Client),
+		unRegister: make(chan *Client),
+		broadcast:  make(chan []byte),
+	}
+}
+
+func (h *Hub) Run() {
+	for {
+		select {
+		case client := <-h.register:
+			h.clients[client] = true
+		case client := <-h.unRegister:
+			if _, ok := h.clients[client]; ok {
+				delete(h.clients, client)
+				close(client.Send)
+			}
+		case message := <-h.broadcast:
+			for client := range h.clients {
+				select {
+				case client.Send <- message:
+				default:
+					close(client.Send)
+					delete(h.clients, client)
+				}
+			}
+		}
+	}
+}
